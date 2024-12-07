b66bcc13b1612fe22b7731d9096d964441110118
Sahil <sahil2215ali@gmail.com>
2024-12-08 05:26:05 +0530

    pushing files

--- src/utils/config.ts
+++
@@ -1,8 +0,0 @@
-export const getChainInfo = (chain: string) => {
-  const chains: { [key: string]: { rpcUrl: string; chainId: number } } = {
-    polygon: { rpcUrl: "https://polygon-rpc.com", chainId: 137 },
-    ethereum: { rpcUrl: "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID", chainId: 1 },
-  };
-
-  return chains[chain] || chains["polygon"];
-};
